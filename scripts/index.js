4cdf2e647068c882516f1a41f2ef454d98f0aaf0
Ruslan Kholikov <kholikov.rus@mail.ru>
2024-10-30 17:56:38 +0300

    Первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+const createCard = (data, deleteCallBack) => {
+    const cardCopy = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardCopy.querySelector('.card__image').src = data.link;
+    cardCopy.querySelector('.card__image').alt = data.name;
+    cardCopy.querySelector('.card__title').textContent = data.name;
+    cardCopy
+        .querySelector('.card__delete-button')
+        .addEventListener('click', deleteCallBack);
+        
+    return cardCopy;
+};
 
 // @todo: Функция удаления карточки
+const deleteCard = (event) => event.target.closest('.card').remove();
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(data => cardContainer.append(createCard(data, deleteCard)));
